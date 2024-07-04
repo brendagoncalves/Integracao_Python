af315a46f06091df91af02e22466367e1ca33fa9
Brenda Gonçalves <brendagrds20@gmail.com>
2024-07-04 12:39:47 -0300

    Update date_calculate.py

--- date_calculate.py
+++ date_calculate.py
@@ -3,24 +3,17 @@ import datetime
 
 
 
-
 def calculate_date():
-  week_day = datetime.date.today().weekday()
+    week_day = datetime.date.today().weekday()
 
-  if week_day == 0: #segunda
-    return(datetime.date.today() + datetime.timedelta(-3))   
-
-  if week_day == 1: #terça
-    return(datetime.date.today() + datetime.timedelta(-1))
-
-  if week_day == 2: #quarta
-    return(datetime.date.today() + datetime.timedelta(-1))
-
-  if week_day == 3: #quinta
-    return(datetime.date.today() + datetime.timedelta(-1))
-
-  if week_day == 4: #sexta
-    return(datetime.date.today() + datetime.timedelta(-1))
+    if week_day == 0: #segunda
+        return(datetime.date.today() + datetime.timedelta(-3)), (datetime.date.today() + datetime.timedelta(-1))
+    else:
+        return(datetime.date.today() + datetime.timedelta(-1)), (datetime.date.today() + datetime.timedelta(-1))
 
 
-dt_start = calculate_date().strftime('%Y-%m-%d')
+dt_start,dt_end = calculate_date()
+
+dt_today = datetime.date.today().strftime('%d/%m/%Y')
+
+
